253d2b6fd3170df7e14b74b65c46a497daedd71b
Justin Demetroff <demetroffj@gmail.com>
2013-12-20 13:43:27 -0500

    added comments

--- keyring.py
+++ keyring.py
@@ -1,9 +1,17 @@
+#db location localhost or IP
 db_host = ""
+#db username with appropriate permissions (select, insert, create)
 db_user = ""
+#matching username password
 db_pass = ""
+#db name ie(HT_Nerd)
 db_name = ""
 
+#twitter oAuth access token
 t_at = ""
+#twitter oAuth access secret
 t_ats = ""
+#twitter consumer key
 t_c = ""
+#twitter consumer secret
 t_cs = ""
