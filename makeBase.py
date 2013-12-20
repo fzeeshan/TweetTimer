06c4828460b65c8187c3fbafa09e7a0e43d284b0
Justin Demetroff <demetroffj@gmail.com>
2013-12-20 13:04:34 -0500

    Update makeBase.py

--- makeBase.py
+++ makeBase.py
@@ -1,7 +1,7 @@
 import MySQLdb
 import keyring
 
-db = MySQLdb.connect(host=keyring.db_host, user=keyring.db_user, passwd=keyring.db_pass, db=keyring.db_name)
+db = MySQLdb.connect(host=keyring.db_host, user=keyring.db_user, passwd=keyring.db_pass)
 cur = db.cursor()
 
 #Enter SQL Stmt
@@ -12,15 +12,18 @@ cur.execute("""
 	CREATE TABLE Tweets
 	(
 		tweetIndex int AUTO_INCREMENT PRIMARY KEY,
-		tweetID int UNIQUE, hashTag varchar(100),
-		userID int, createdTime varchar(100),
-		tweetText varchar(200), reTweet int
+		tweetID bigint(255) UNIQUE,
+		hashTag varchar(100),
+		userID bigint(255), 
+		createdTime varchar(100),
+		tweetText varchar(200), 
+		reTweet int
 	);
 
 	CREATE TABLE Users
 	(
 		userIndex int AUTO_INCREMENT PRIMARY KEY,
-	 	userID int,
+	 	userID bigint(255),
 	 	userImage varchar(500), 
 	 	userScreenName varchar(200),
 	 	userRealName varchar(200)
